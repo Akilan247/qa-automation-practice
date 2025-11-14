b5018cd356530f9b48498c5a9b92e902d12c9f92
Akilan247 <146917836+Akilan247@users.noreply.github.com>
2025-11-14 10:34:44 +0530

    Updated

--- Automation-testing/src/main/java/org/example/LambdaExp/Ex1/Main.java
+++ Automation-testing/src/main/java/org/example/LambdaExp/Ex1/Main.java
@@ -8,9 +8,6 @@ public class Main {
     public static void main(String[] args) {
         HotelServices hotelServices = new HotelServices();
 
-
-
-
         List<Hotel> threetarHotels = hotelServices.filterHotels(new Predicate() {
 
             @Override
