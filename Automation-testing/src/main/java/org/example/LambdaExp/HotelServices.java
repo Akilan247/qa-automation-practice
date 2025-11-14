6977629b5bd975bc08dd9fef818f423641f56537
Akilan247 <146917836+Akilan247@users.noreply.github.com>
2025-11-14 10:37:01 +0530

    Updated

--- Automation-testing/src/main/java/org/example/LambdaExp/HotelServices.java
+++
@@ -1,9 +0,0 @@
-package org.example.LambdaExp;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class HotelServices {
-    List<Hotel> hotels = new ArrayList<>();
-
-}
--- Automation-testing/src/main/java/org/example/LambdaExp/Main.java
+++
@@ -1,8 +0,0 @@
-package org.example.LambdaExp;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Hotel hotel = new Hotel(101,2000,HotelType.THREE_STAR);
-    }
-}
